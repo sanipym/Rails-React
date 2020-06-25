cd5303a6bd1dde5ea74f41af184696c197f6e804
sanipym <sanipym@gmail.com>
2020-06-26 03:21:46 +0530

    Add the React gem to the Project

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,5 @@
+#app/controllers/site_controller.rb
+class SiteController < ApplicationController
+	def index
+	end
+end		
---
+++ app/javascript/components/post.jsx
@@ -0,0 +1,6 @@
+window.Post = createReactClass({
+	render: function() {
+	  return <h1>{this.props.title}</h1>
+	}
+  })
+  
